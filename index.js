151015b80388af427d323e7be3e01c7c29cd66a6
crob127 <164558528+crob127@users.noreply.github.com>
2024-06-10 22:24:48 -0400

    fixed some bugs and added example svg files generated from the app

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ inquirer.prompt(questions).then(answers => {
 
     const svgLogo = `
         <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        ${shapeObj.render()}
+        ${shapeObject.render()}
         <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
         </svg>`;
 
         fs.writeFileSync('logo.svg', svgLogo.trim());
         console.log('Generated logo.svg successfully!')
-})
+});
